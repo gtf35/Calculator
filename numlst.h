a2eba5451ff4ff8fa60901d0984b50b4fd35883d
lxx4380 <lxx4380@gmail.com>
2017-02-26 00:22:35 +0800

    update numlst.h

--- numlst.h
+++ numlst.h
@@ -1,8 +1,59 @@
 #pragma once
 
+#ifndef __cplusplus
+
+#ifndef NUMLST
+#define NUMLST
+
+#ifndef _STDBOOL
+
+typedef _Bool bool;
+
+#endif // _STDBOOL
+
 typedef struct {
 	void*stptr;
 	char num;
 	void*nxptr;
 } numlst;
 
+inline numlst**newlst(void) {
+	numlst*ptr = malloc(sizeof(numlst)), **nptr = &ptr;
+	(*nptr)->stptr = *nptr;
+	(*nptr)->nxptr = *nptr;
+	return nptr;
+}
+
+inline bool isend(numlst**ptr) {
+	if ((*ptr)->stptr == *ptr || (*ptr)->nxptr == *ptr)return 1;
+	return 0;
+}
+
+inline int getnum(numlst**ptr) {
+	return (*ptr)->num;
+}
+
+inline void setnum(numlst**ptr, int num) {
+	(*ptr)->num = num;
+}
+
+inline void toup(numlst**ptr) {
+	*ptr = (*ptr)->stptr;
+}
+
+inline void todown(numlst**ptr) {
+	*ptr = (*ptr)->nxptr;
+}
+
+inline void totop(numlst**ptr) {
+	do toup(ptr);
+	while (!isend(ptr));
+}
+
+#endif // NUMLST
+
+#else // __cplusplus
+
+#error This is a C program.
+
+#endif // __cplusplus

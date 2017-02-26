e47e1ea8605df4b8f886217cb22ec12e4bb1243d
lxx4380 <lxx4380@gmail.com>
2017-02-26 23:06:54 +0800

    update numlst.h, add test.c

--- numlst.h
+++ numlst.h
@@ -21,11 +21,21 @@ inline numlst**newlst(void) {
 	numlst*ptr = malloc(sizeof(numlst)), **nptr = &ptr;
 	(*nptr)->stptr = *nptr;
 	(*nptr)->nxptr = *nptr;
-	return nptr;
+	return *nptr;
 }
 
 inline bool isend(numlst**ptr) {
 	if ((*ptr)->stptr == *ptr || (*ptr)->nxptr == *ptr)return 1;
+	return 0;
+}
+
+inline bool istop(numlst**ptr) {
+	if ((*ptr)->stptr == *ptr)return 1;
+	return 0;
+}
+
+inline bool isbottom(numlst**ptr) {
+	if ((*ptr)->nxptr == *ptr)return 1;
 	return 0;
 }
 
@@ -48,6 +58,62 @@ inline void todown(numlst**ptr) {
 inline void totop(numlst**ptr) {
 	do toup(ptr);
 	while (!isend(ptr));
+}
+
+inline void delup(numlst**ptr) {
+	toup(ptr);
+	if (isend(ptr)) {
+		todown(ptr);
+		free((*ptr)->stptr);
+		(*ptr)->stptr = *ptr;
+	}
+	else {
+		todown(ptr);
+		numlst*ststptr = ((numlst*)(*ptr)->stptr)->stptr;
+		ststptr->nxptr = *ptr;
+		(*ptr)->stptr = ststptr;
+	}
+}
+
+inline void deldown(numlst**ptr) {
+	todown(ptr);
+	if (isend(ptr)) {
+		toup(ptr);
+		free((*ptr)->nxptr);
+		(*ptr)->nxptr = *ptr;
+	}
+	else {
+		toup(ptr);
+		numlst*nxnxptr = ((numlst*)(*ptr)->nxptr)->nxptr;
+		nxnxptr->stptr = *ptr;
+		(*ptr)->nxptr = nxnxptr;
+	}
+}
+
+inline void addup(numlst**ptr) {
+	numlst*nptr = malloc(sizeof(numlst));
+	nptr->nxptr = *ptr;
+	if (istop(ptr)) {
+		nptr->stptr = nptr;
+	}
+	else {
+		((numlst*)((*ptr)->stptr))->nxptr = nptr;
+		nptr->stptr = (numlst*)((*ptr)->stptr);
+	}
+	(*ptr)->stptr = nptr;
+}
+
+inline void adddown(numlst**ptr) {
+	numlst*nptr = malloc(sizeof(numlst));
+	nptr->stptr = *ptr;
+	if (isbottom(ptr)) {
+		nptr->nxptr = nptr;
+	}
+	else {
+		((numlst*)((*ptr)->nxptr))->stptr = nptr;
+		nptr->nxptr = (numlst*)((*ptr)->nxptr);
+	}
+	(*ptr)->nxptr = nptr;
 }
 
 #endif // NUMLST
---
+++ test.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#include"numlst.h"
+
+int main(void) {
+	numlst**ptr = newlst();
+	printf("address:0x%p\n", *ptr);
+	printf("istop():%d isbottom():%d isend():%d\n", istop(ptr), isbottom(ptr), isend(ptr));
+	setnum(ptr, 1);
+	printf("set num to 1.\nnum is now %d.\n", getnum(ptr));
+	setnum(ptr, 2);
+	printf("set num to 2.\nnum is now %d.\n", getnum(ptr));
+	addup(ptr);
+	printf("addup()\n");
+	printf("istop():%d isbottom():%d isend():%d\n", istop(ptr), isbottom(ptr), isend(ptr));
+	getchar();
+}
